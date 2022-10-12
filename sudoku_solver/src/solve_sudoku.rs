66b56a86509b73d4f54e2468a34f42eacd0b6239
Mendel <mendeljacks@gmail.com>
2022-10-11 22:41:44 -0400

    progress

--- sudoku_solver/src/solve_sudoku.rs
+++ sudoku_solver/src/solve_sudoku.rs
@@ -1,20 +1,55 @@
 use std::collections::HashSet;
 
 pub struct Solution {}
-
 enum Cell {
-    Char,
-    Set,
+    SingleValue(char),
+    MultiValue(HashSet<char>),
 }
+type Board = Vec<Vec<char>>;
+type Progress = Vec<Vec<Cell>>;
+
+fn board_to_progress(board: &mut Board) -> Progress {
+    board
+        .into_iter()
+        .map(|el| {
+            el.into_iter()
+                .map(|chr| {
+                    if *chr == '.' {
+                        Cell::MultiValue(['1', '2', '3', '4', '5', '6', '7', '8', '9'].into())
+                    } else {
+                        Cell::SingleValue(*chr)
+                    }
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn remove_from_row(progress: &mut Progress, row: usize, chr: &char) -> Progress {
+    for (j, _) in (&progress[row]).into_iter().enumerate() {
+        progress[row][j] = match progress[row][j] {
+            Cell::SingleValue(chr) => Cell::SingleValue(chr),
+            Cell::MultiValue(mut val) => {
+                val.remove(&chr);
+                Cell::MultiValue(val)
+            }
+        };
+    }
+    *progress
+}
+fn remove_from_col(progress: &mut Progress, col: i32, chr: char) {}
+fn remove_from_sub(progress: &mut Progress, sub: i32, chr: char) {}
 
 impl Solution {
     pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
-        let progress: &mut Vec<Vec<Cell>> = board;
-        for (i, row) in board.iter().enumerate() {
+        let mut progress: Vec<Vec<Cell>> = board_to_progress(board);
+
+        for (i, row) in (&progress).iter().enumerate() {
             for (j, mut cell) in row.iter().enumerate() {
-                if *cell == '.' {
-                    cell = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-                }
+                progress = match cell {
+                    Cell::SingleValue(chr) => remove_from_row(&mut progress, i, chr),
+                    // Cell::MultiValue(val) => remove_from_row(&mut progress, i, &'a'.into()),
+                };
             }
         }
 
