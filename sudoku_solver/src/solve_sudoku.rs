b0b782b0dfb6d2553a375ad3273376c8355efe2b
Mendel <mendeljacks@gmail.com>
2022-10-13 10:32:50 -0400

    progress

--- sudoku_solver/src/solve_sudoku.rs
+++ sudoku_solver/src/solve_sudoku.rs
@@ -30,7 +30,45 @@ fn remove_from_sub(grid: &mut Options, row: usize, col: usize, num: u32) {
     }
 }
 
+fn solved(board: &Board) -> bool {
+    for row in board.into_iter() {
+        for cell in row.into_iter() {
+            if *cell == '.' {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+fn remove_singletons(board: &mut Board, options: &mut Options) {
+    // ssingletons and upgrade entries removing any additional invalid possibilities
+    for (i, row) in board.into_iter().enumerate() {
+        for (j, mut cell) in row.iter_mut().enumerate() {
+            if options[i][j].len() == 1 {
+                let u = options[i][j].drain().next().unwrap();
+                let mut chr = std::char::from_u32(u).unwrap();
+                cell = &mut chr;
+                remove_from_row(options, i, u);
+                remove_from_col(options, j, u);
+                remove_from_sub(options, i, j, u);
+            }
+        }
+    }
+}
+
+fn remove_preemptive(board: &mut Board, options: &mut Options) {
+    // look for items with cardinality matching number of boxes preemptive set
+    // look for items with only one option for a number in the row, col, sub
+    for (i, row) in board.into_iter().enumerate() {
+        for (j, mut cell) in row.iter_mut().enumerate() {
+            // number of cells with equal value match number of values
+        }
+    }
+}
+
 fn walk_board(board: &mut Board, options: &mut Options) {
+    // initial scan
     for (i, row) in board.into_iter().enumerate() {
         for (j, cell) in row.into_iter().enumerate() {
             if *cell != '.' {
@@ -43,21 +81,10 @@ fn walk_board(board: &mut Board, options: &mut Options) {
         }
     }
 
-    // look for items with one possibility and upgrade entries removing any additional invalid possibilities
-    for (i, row) in board.into_iter().enumerate() {
-        for (j, cell) in row.into_iter().enumerate() {
-            if options[i][j].len() == 1 {
-                board[i][j] = std::char::from_u32(options[i][j].take(&1).unwrap()).unwrap();
-                options[i][j].drain();
-                remove_from_row(options, i, (*cell).to_digit(10).unwrap());
-                remove_from_col(options, j, (*cell).to_digit(10).unwrap());
-                remove_from_sub(options, i, j, (*cell).to_digit(10).unwrap());
-            }
-        }
+    while !solved(board) {
+        remove_singletons(board, options);
+        remove_preemptive(board, options)
     }
-
-    // look for items with cardinality matching number of boxes
-    // look for items with only one option for a number in the row, col, sub
 }
 
 impl Solution {
@@ -65,7 +92,7 @@ impl Solution {
         // for each element of the board if empty make all options
         let mut options: Options = vec![vec![HashSet::from([1, 2, 3, 4, 5, 6, 7, 8, 9]); 9]; 9];
 
-        walk_board(&mut board, &mut options);
+        walk_board(board, &mut options);
 
         for i in options {
             println!("{:?}", i);
