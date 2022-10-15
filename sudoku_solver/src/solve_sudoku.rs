bf3d0976bafd3d4dd726612030af6cbf148753b7
Mendel <mendeljacks@gmail.com>
2022-10-15 01:53:05 -0400

    progress

--- sudoku_solver/src/solve_sudoku.rs
+++ sudoku_solver/src/solve_sudoku.rs
@@ -41,19 +41,40 @@ fn handle_preemptive_sets(options: &mut Options) {
         }
     }
 
-    for (key, vals) in lookup {
-        // If same row preemptive set
+    for (key, vals) in &lookup {
+        // add other keys in lookup object which have subset of chars
+        let subset_keys: Vec<String> = lookup
+            .keys()
+            .filter(|el| {
+                let el2: String = el.to_string();
+                el2 != *key && el2.chars().all(|c| key.contains(c))
+            })
+            .map(|el| el.to_string())
+            .collect();
+        let mut subset_vals = subset_keys
+            .iter()
+            .map(|el| lookup.get(el).unwrap().to_vec())
+            .collect::<Vec<Vec<(usize, usize)>>>();
+        // let vals be unique of vals with appended subset_vals
+        let vals = vals
+            .append(&mut subset_vals)
+            .collect::<Vec<(usize, usize)>>()
+            .into_iter()
+            .collect::<HashSet<(usize, usize)>>()
+            .into_iter()
+            .collect::<Vec<(usize, usize)>>();
         for i in 0..9 {
-            if key.len() == vals.iter().filter(|el| el.0 == i).count() {
-                let characters = key.chars();
-                for chr in characters {
-                    let num = chr.to_digit(10).unwrap();
+            let characters = key.chars();
+            for chr in characters {
+                let num = chr.to_digit(10).unwrap();
 
-                    let row = &mut options[i];
-                    for (j, cell) in row.iter_mut().enumerate() {
-                        if !vals.contains(&(i, j)) {
-                            cell.remove(&num);
-                        }
+                let row = &mut options[i];
+                for (j, cell) in row.iter_mut().enumerate() {
+                    // If same row preemptive set
+                    if key.len() == vals.iter().filter(|el| el.0 == i).count()
+                        && !vals.contains(&(i, j))
+                    {
+                        cell.remove(&num);
                     }
                 }
             }
@@ -103,6 +124,58 @@ fn handle_preemptive_sets(options: &mut Options) {
     }
 
     // look for items with only one option for a number in the row, col, sub
+    for i in 0..9 {
+        for digit in 0..9 {
+            let mut count = 0;
+            let mut last = (0, 0);
+            for (j, cell) in options[i].iter().enumerate() {
+                if cell.contains(&(digit + 1)) {
+                    count += 1;
+                    last = (i, j);
+                }
+            }
+            if count == 1 {
+                options[last.0][last.1] = HSu32::from([digit + 1]);
+            }
+        }
+    }
+
+    for j in 0..9 {
+        for digit in 0..9 {
+            let mut count = 0;
+            let mut last = (0, 0);
+            for (i, row) in options.iter().enumerate() {
+                let cell = &row[j];
+                if cell.contains(&(digit + 1)) {
+                    count += 1;
+                    last = (i, j);
+                }
+            }
+            if count == 1 {
+                options[last.0][last.1] = HSu32::from([digit + 1]);
+            }
+        }
+    }
+
+    for i in 0..3 {
+        for j in 0..3 {
+            for digit in 0..9 {
+                let mut count = 0;
+                let mut last = (0, 0);
+                for (ii, row) in options.iter().enumerate() {
+                    for (jj, cell) in row.iter().enumerate() {
+                        if cell.contains(&(digit + 1)) && ii / 3 == i && jj / 3 == j {
+                            count += 1;
+                            last = (ii, jj);
+                        }
+                    }
+                }
+                if count == 1 {
+                    options[last.0][last.1] = HSu32::from([digit + 1]);
+                }
+            }
+        }
+    }
 }
 
 fn initial_scan(board: &Board, options: &mut Options) {
