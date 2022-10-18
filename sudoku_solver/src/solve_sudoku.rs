639e29f8eabcaf56f9b20c7bac12ba65d2f64f9e
Mendel <mendeljacks@gmail.com>
2022-10-18 02:11:30 -0400

    works

--- sudoku_solver/src/solve_sudoku.rs
+++ sudoku_solver/src/solve_sudoku.rs
@@ -5,13 +5,6 @@ type HSu32 = HashSet<u32>;
 type Options = Vec<Vec<HSu32>>;
 type Board = Vec<Vec<char>>;
 
-struct Guess {
-    row: usize,
-    col: usize,
-    value: u32,
-    options: Options,
-}
-
 fn solved(options: &Options) -> bool {
     for row in options.into_iter() {
         for cell in row.into_iter() {
@@ -33,9 +26,7 @@ fn hsu32_to_string(hs: &HashSet<u32>) -> String {
         .collect()
 }
 
-fn handle_preemptive_sets(options: &mut Options) {
-    //  hashmap = { "235": [(i,j), (i2,j2)] };
-
+fn make_lookup(options: &Options) -> HashMap<String, Vec<(usize, usize)>> {
     let mut lookup: HashMap<String, Vec<(usize, usize)>> = HashMap::new();
 
     for (i, row) in options.into_iter().enumerate() {
@@ -47,6 +38,12 @@ fn handle_preemptive_sets(options: &mut Options) {
             lookup.entry(string2).or_insert(vec![]).push((i, j));
         }
     }
+    lookup
+}
+
+fn handle_preemptive_sets(options: &mut Options) -> bool {
+    let mut changed = false;
+    let lookup = make_lookup(&options);
 
     for (key, values) in &lookup {
         // add other keys in lookup object which have subset of chars
@@ -80,8 +77,10 @@ fn handle_preemptive_sets(options: &mut Options) {
                     // If same row preemptive set
                     if key.len() == vals.iter().filter(|el| el.0 == i).count()
                         && !vals.contains(&(i, j))
+                        && cell.contains(&num)
                     {
                         cell.remove(&num);
+                        changed = true;
                     }
                 }
             }
@@ -96,8 +95,9 @@ fn handle_preemptive_sets(options: &mut Options) {
 
                     for (i, row) in options.iter_mut().enumerate() {
                         let cell = &mut row[j];
-                        if !vals.contains(&(i, j)) {
+                        if !vals.contains(&(i, j)) && cell.contains(&u) {
                             cell.remove(&u);
+                            changed = true;
                         }
                     }
                 }
@@ -119,8 +119,13 @@ fn handle_preemptive_sets(options: &mut Options) {
 
                         for (ii, row) in options.iter_mut().enumerate() {
                             for (jj, cell) in row.iter_mut().enumerate() {
-                                if !vals.contains(&(ii, jj)) && ii / 3 == i && jj / 3 == j {
+                                if cell.contains(&u)
+                                    && !vals.contains(&(ii, jj))
+                                    && ii / 3 == i
+                                    && jj / 3 == j
+                                {
                                     cell.remove(&u);
+                                    changed = true;
                                 }
                             }
                         }
@@ -141,8 +146,9 @@ fn handle_preemptive_sets(options: &mut Options) {
                     last = (i, j);
                 }
             }
-            if count == 1 {
+            if count == 1 && options[last.0][last.1].len() > 1 {
                 options[last.0][last.1] = HSu32::from([digit + 1]);
+                changed = true;
             }
         }
     }
@@ -158,8 +164,9 @@ fn handle_preemptive_sets(options: &mut Options) {
                     last = (i, j);
                 }
             }
-            if count == 1 {
+            if count == 1 && options[last.0][last.1].len() > 1 {
                 options[last.0][last.1] = HSu32::from([digit + 1]);
+                changed = true;
             }
         }
     }
@@ -177,12 +184,15 @@ fn handle_preemptive_sets(options: &mut Options) {
                         }
                     }
                 }
-                if count == 1 {
+                if count == 1 && options[last.0][last.1].len() > 1 {
                     options[last.0][last.1] = HSu32::from([digit + 1]);
+                    changed = true;
                 }
             }
         }
     }
+
+    changed
 }
 
 fn initial_scan(board: &Board, options: &mut Options) {
@@ -197,39 +207,112 @@ fn initial_scan(board: &Board, options: &mut Options) {
     }
 }
 
+fn is_invalid(options: Options) -> bool {
+    for row in options.iter() {
+        for cell in row.iter() {
+            if cell.len() == 0 {
+                return true;
+            }
+        }
+    }
+
+    // check for duplicates in row col or box
+    for i in 0..9 {
+        let mut row = HashSet::new();
+        let mut col = HashSet::new();
+        let mut box_ = HashSet::new();
+        for j in 0..9 {
+            let cell = &options[i][j];
+            if cell.len() == 1 {
+                if row.contains(cell.iter().next().unwrap()) {
+                    return true;
+                }
+                row.insert(cell.iter().next().unwrap());
+            }
+
+            let cell = &options[j][i];
+            if cell.len() == 1 {
+                if col.contains(cell.iter().next().unwrap()) {
+                    return true;
+                }
+                col.insert(cell.iter().next().unwrap());
+            }
+
+            let cell = &options[i / 3 * 3 + j / 3][i % 3 * 3 + j % 3];
+            if cell.len() == 1 {
+                if box_.contains(cell.iter().next().unwrap()) {
+                    return true;
+                }
+                box_.insert(cell.iter().next().unwrap());
+            }
+        }
+    }
+
+    false
+}
+
+fn print(options: &Options) {
+    print!("\x1B[2J\x1B[1;1H");
+
+    for (i, row) in options.into_iter().enumerate() {
+        if i % 3 == 0 {
+            println!("-------------------------------------------------------------------------------------------------");
+        }
+        println!(
+            "{0: <10} {1: <10} {2: <10} | {3: <10} {4: <10} {5: <10} | {6: <10} {7: <10} {8: <10}",
+            hsu32_to_string(&row[0]),
+            hsu32_to_string(&row[1]),
+            hsu32_to_string(&row[2]),
+            hsu32_to_string(&row[3]),
+            hsu32_to_string(&row[4]),
+            hsu32_to_string(&row[5]),
+            hsu32_to_string(&row[6]),
+            hsu32_to_string(&row[7]),
+            hsu32_to_string(&row[8])
+        );
+    }
+}
+
 fn walk_board(board: &mut Board, options: &mut Options) {
     initial_scan(board, options);
-    let guesses: Vec<Guess> = vec![];
-    let changed = false;
+    let mut guesses: Vec<Options> = vec![];
+    let mut changed;
 
     while !solved(options) {
-        print!("\x1B[2J\x1B[1;1H");
+        changed = handle_preemptive_sets(options);
 
-        println!("=====================");
-        for (i, row) in options.into_iter().enumerate() {
-            if i % 3 == 0 {
-                println!("-------------------------------------------------------------------------------------------------");
+        if changed == false {
+            // find the element with the least options
+            let mut min = 10;
+            let mut min_pos = (0, 0);
+            for (i, row) in options.into_iter().enumerate() {
+                for (j, cell) in row.into_iter().enumerate() {
+                    if cell.len() < min && cell.len() > 1 {
+                        min = cell.len();
+                        min_pos = (i, j);
+                    }
+                }
             }
-            println!(
-                "{0: <10} {1: <10} {2: <10} | {3: <10} {4: <10} {5: <10} | {6: <10} {7: <10} {8: <10}",
-                hsu32_to_string(&row[0]),
-                hsu32_to_string(&row[1]),
-                hsu32_to_string(&row[2]),
-                hsu32_to_string(&row[3]),
-                hsu32_to_string(&row[4]),
-                hsu32_to_string(&row[5]),
-                hsu32_to_string(&row[6]),
-                hsu32_to_string(&row[7]),
-                hsu32_to_string(&row[8])
-            );
+
+            let new_guesses = options[min_pos.0][min_pos.1]
+                .iter()
+                .map(|&x| {
+                    let mut new_options = options.clone();
+                    new_options[min_pos.0][min_pos.1].drain();
+                    new_options[min_pos.0][min_pos.1].insert(x);
+                    new_options
+                })
+                .collect::<Vec<_>>();
+            guesses.append(&mut new_guesses.clone());
+            let last_guess = guesses.pop().unwrap();
+            *options = last_guess;
         }
-        println!("=====================");
-
-        handle_preemptive_sets(options);
-
-        std::thread::sleep(std::time::Duration::from_millis(500));
+        if is_invalid(options.to_vec()) {
+            let last_guess = guesses.pop().unwrap();
+            *options = last_guess;
+        }
+        // std::thread::sleep(std::time::Duration::from_millis(500));
     }
-    println!("Solved!");
 
     // loop through options and set board
     for (i, row) in options.into_iter().enumerate() {
@@ -245,5 +328,6 @@ impl Solution {
         let mut options: Options = vec![vec![HashSet::from([1, 2, 3, 4, 5, 6, 7, 8, 9]); 9]; 9];
 
         walk_board(board, &mut options);
+        print(&options);
     }
 }
